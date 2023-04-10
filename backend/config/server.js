b8931dbcce77cd1578ab24ebe3d065633241ce02
Mikhail <hramtsov13@mail.ru>
2023-04-10 15:51:57 +0300

    backend: add host url

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  url: env("PUBLIC_URL", "http://localhost:1337"),
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
   },
+  url: "https://blog.hr13.ru",
 });
